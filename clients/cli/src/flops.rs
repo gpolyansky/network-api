d697f18439ce35402fc72e657e4133385cee47e5
Jerry <f0r3s1@gmail.com>
2025-02-19 09:20:28 +0300

    my fix

--- clients/cli/src/flops.rs
+++ clients/cli/src/flops.rs
@@ -1,12 +1,13 @@
-// Measure the FLOPS of the CPU
-// use num_cpus;
 use rayon::prelude::*;
 use std::time::Instant;
+use rand::Rng; 
 
 const NTESTS: u64 = 1_000_000;
 const OPERATIONS_PER_ITERATION: u64 = 4; // sin, add, multiply, divide
 
 pub fn measure_flops() -> f32 {
+    let multiplier: u64 = rand::thread_rng().gen_range(5..=20);
+
     let num_cores = num_cpus::get() as u64;
     let start = Instant::now();
 
@@ -15,18 +16,18 @@ pub fn measure_flops() -> f32 {
         .map(|_| {
             let mut x: f64 = 1.0;
             for _ in 0..NTESTS {
-                x = (x.sin() + 1.0) * 0.5 / 1.1;
+                for _ in 0..multiplier {
+                    x = (x.sin() + 1.0) * 0.5 / 1.1;
+                }
             }
-            // Prevent compiler from optimizing out the loop
             if x.is_nan() {
                 println!("This should never happen");
             }
-            NTESTS * OPERATIONS_PER_ITERATION
+            NTESTS * OPERATIONS_PER_ITERATION * multiplier
         })
         .sum::<u64>() as f64;
 
     let duration = start.elapsed();
-
     let flops = total_flops / duration.as_secs_f64();
     flops as f32
 }
